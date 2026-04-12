bf79f7e1fa9c62fa4d91520986cdb06d78fbde78
Andres-2707 <andresjvj652@gmail.com>
2026-04-12 17:42:09 -0600

    Semana1

---
+++ TiendaAndresVJ/src/main/java/TiendaAndres/JVJ/TiendaAndresApplication.java
@@ -0,0 +1,13 @@
+package TiendaAndres.JVJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAndresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAndresApplication.class, args);
+	}
+
+}
